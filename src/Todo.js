0ed2ea8f24a00b5f1335b72e0482f7d7aabf0b9e
TrailblaZer132 <uditjeet@gmail.com>
2021-07-17 03:16:46 +0530

    Delete icon modified

--- src/Todo.js
+++ src/Todo.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import './Todo.css'
-import { List,ListItem,ListItemText,ListItemAvatar, Avatar} from '@material-ui/core';
+import { List,ListItem,ListItemText,ListItemAvatar, Avatar, Container, IconButton} from '@material-ui/core';
 import db from './firebase'
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 
 function Todo(props) {
     return (
-        <List className="todo_List">
-            <ListItem>
-                <ListItemAvatar>
-                <Avatar>
-                    <h3>#</h3>
-                </Avatar>
-                </ListItemAvatar>
-                <ListItemText primary={props.todo.todo} secondary="Deadline ⏰" />
-            </ListItem>
-            <DeleteForeverIcon onClick={event=>db.collection('todos').doc(props.todo.id).delete()} />
-        </List>
+        <Container maxWidth="sm">
+            <List className="todo_List">
+                <ListItem>
+                    <ListItemAvatar>
+                    <Avatar>
+                        <h3>#</h3>
+                    </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={props.todo.todo} secondary="Deadline ⏰" />
+                    {/* <Button onClick={event=>db.collection('todos').doc(props.todo.id).delete()}>
+                            <DeleteForeverIcon/>
+                    </Button> */}
+                    <IconButton aria-label="delete"  onClick={event=>db.collection('todos').doc(props.todo.id).delete()}>
+                        <DeleteForeverIcon fontSize="large" />
+                    </IconButton>
+                </ListItem>
+            </List>
+        </Container>
     )
 }
 
